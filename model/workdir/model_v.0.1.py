a3079ec633c4908a3e80b598c82a4a6e0cbc1db7
kolombo2407 <e.kolomiets@aol.com>
2023-04-04 14:14:34 -0700

    starting model

---
+++ model/workdir/model_v.0.1.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+import numpy as np
+import scipy
+import pickle
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
+
